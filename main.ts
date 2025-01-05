76a2e84c001f79d9b7c6169acc6995775c525266
will <will413028@gmail.com>
2025-01-05 20:21:57 +0800

    ✨ [feat] add fetch forvo api

--- main.ts
+++ main.ts
@@ -2,80 +2,101 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PronunciationDownloaderSettings {
+	apiKey: string;
+	downloadPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PronunciationDownloaderSettings = {
+	apiKey: '',
+	downloadPath: 'pronunciations'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ForvoAPI {
+	private apiKey: string;
+	private baseUrl = 'https://apifree.forvo.com';
+
+	constructor(apiKey: string) {
+		this.apiKey = apiKey;
+	}
+
+	async searchWord(word: string, language: string = 'en') {
+		const url = `${this.baseUrl}/key/${this.apiKey}/format/json/action/word-pronunciations/word/${encodeURIComponent(word)}/language/${language}`;
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Failed to fetch pronunciations: ${response.statusText}`);
+		}
+		return await response.json();
+	}
+
+	async downloadPronunciation(url: string): Promise<ArrayBuffer> {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Failed to download pronunciation: ${response.statusText}`);
+		}
+		return await response.arrayBuffer();
+	}
+}
+
+export default class PronunciationDownloader extends Plugin {
+	settings: PronunciationDownloaderSettings;
+	private forvoApi: ForvoAPI;
+
+	async downloadPronunciation(word: string) {
+		try {
+			const result = await this.forvoApi.searchWord(word);
+			if (result.items && result.items.length > 0) {
+				const pronunciation = result.items[0];
+				const audioData = await this.forvoApi.downloadPronunciation(pronunciation.pathmp3);
+				
+				// Save the file in the vault
+				const fileName = `${this.settings.downloadPath}/${word}_${pronunciation.id}.mp3`;
+				await this.app.vault.createBinary(fileName, audioData);
+				
+				new Notice(`Downloaded pronunciation for "${word}"`);
+			} else {
+				new Notice(`No pronunciation found for "${word}"`);
+			}
+		} catch (error) {
+			console.error('Error downloading pronunciation:', error);
+			new Notice(`Failed to download pronunciation: ${error.message}`);
+		}
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.forvoApi = new ForvoAPI(this.settings.apiKey);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// Add ribbon icon for quick access
+		this.addRibbonIcon('sound', 'Download Pronunciation', () => {
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (activeView) {
+				const selection = activeView.editor.getSelection();
+				if (selection) {
+					this.downloadPronunciation(selection);
+				} else {
+					new Notice('Please select a word to download its pronunciation');
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Add command to download pronunciation
+		this.addCommand({
+			id: 'download-pronunciation',
+			name: 'Download pronunciation for selected word',
+			editorCallback: (editor: Editor) => {
+				const selection = editor.getSelection();
+				if (selection) {
+					this.downloadPronunciation(selection);
+				} else {
+					new Notice('Please select a word to download its pronunciation');
+				}
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add settings tab
+		this.addSettingTab(new PronunciationSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,26 +112,11 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class PronunciationSettingTab extends PluginSettingTab {
+	plugin: PronunciationDownloader;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PronunciationDownloader) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +127,24 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Forvo API Key')
+			.setDesc('Enter your Forvo API key')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter API key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Download Path')
+			.setDesc('Path where pronunciation files will be saved (relative to vault)')
+			.addText(text => text
+				.setPlaceholder('pronunciations')
+				.setValue(this.plugin.settings.downloadPath)
+				.onChange(async (value) => {
+					this.plugin.settings.downloadPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
